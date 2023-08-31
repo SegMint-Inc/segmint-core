e48c03ff28105fd02c04c6176f76d0e757ec350c
ItsCuzzo <alan@cuzzolabs.io>
2023-08-31 21:13:23 +1000

    chore: improved deploy script

--- script/01_Deploy.s.sol
+++ script/01_Deploy.s.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Script.sol";
 import "../test/Base.sol";
 
 contract DeployScript is Base {
@@ -11,10 +10,10 @@ contract DeployScript is Base {
     uint256 adminPrivateKey;
     address admin;
 
-    // uint256 signerPrivateKey;
-    // address signer;
+    uint256 signerPrivateKey;
+    address signer;
 
-    function setUp() public override {
+    function setUp() public {
         deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         deployer = vm.rememberKey(deployerPrivateKey);
 
@@ -29,40 +28,6 @@ contract DeployScript is Base {
 
     function run() public {
         vm.startBroadcast(deployer);
-
-        /// Deploy base registry contracts.
-        signerRegistry = new SignerRegistry({ admin_: admin, signer_: signer });
-        kycRegistry = new KYCRegistry({ admin_: admin, signerRegistry_: ISignerRegistry(signerRegistry) });
-        keys = new Keys({ admin_: admin, uri_: "", kycRegistry_: IKYCRegistry(kycRegistry) });
-        keyExchange = new KeyExchange({ admin_: admin, keys_: IKeys(keys), feeReceiver_: FEE_RECEIVER, weth_: WETH });
-
-        /// Deploy implementation addresses for service factory.
-        saVault = new SAVault();
-        maVault = new MAVault();
-        // safeImplementation = new Safe();
-
-        /// Deploy service factory implementation and proxy.
-        serviceFactoryImplementation = new ServiceFactory();
-        serviceFactoryProxy = new ServiceFactoryProxy({
-            implementation_: address(serviceFactoryImplementation),
-            payload_: abi.encodeWithSelector(
-                ServiceFactory.initialize.selector,
-                admin,
-                address(maVault),
-                address(saVault),
-                address(0),
-                ISignerRegistry(signerRegistry),
-                IKYCRegistry(kycRegistry),
-                IKeys(keys)
-            )
-        });
-
-        /// Proxy the service factory through the service factory proxy.
-        serviceFactoryProxied = ServiceFactory(address(serviceFactoryProxy));
-
-        /// Grant service factory the correct role for vault registration.
-        keys.grantRoles({ user: address(serviceFactoryProxied), roles: FACTORY_ROLE });
-
         vm.stopBroadcast();
     }
 }
