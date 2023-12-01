5a0a2357245e099af386fd6e4ae78faa4a511663
ItsCuzzo <alan@cuzzolabs.io>
2023-12-01 15:44:37 +1100

    bug: update typehasher with correct params

--- src/handlers/TypeHasher.sol
+++ src/handlers/TypeHasher.sol
@@ -12,11 +12,11 @@ abstract contract TypeHasher is EIP712 {
     /// Royalties(address receiver,uint256 fee)
     bytes32 private constant _ROYALTIES_TYPEHASH = 0x2f5057decee872280b232f42dc21db20fd2f34148dcb1f26e39248197261978e;
 
-    /// Order(uint256 price,address maker,address taker,uint256 keyId,uint256 amount,uint256 nonce,uint256 startTime,uint256 endTime,uint256 protocolFee,Royalties[] royalties)
-    bytes32 private constant _ORDER_TYPEHASH = 0x77bb84960a38daa0cd316ff16f7fb0f47c9163fafbb5065fa2ef34176e095aad;
+    /// Order(uint256 price,address maker,address taker,uint256 keyId,uint256 amount,uint256 nonce,uint256 startTime,uint256 endTime,uint256 protocolFee,Royalties[] royalties)Royalties(address receiver,uint256 fee)
+    bytes32 private constant _ORDER_TYPEHASH = 0x181555209a4040dc9b2401944efa8ca3b593b5fc3981fa4b4c4996bcea6c727f;
 
-    /// Bid(address maker,uint256 price,uint256 keyId,uint256 amount,uint256 nonce,uint256 startTime,uint256 endTime,uint256 protocolFee,Royalties[] royalties)
-    bytes32 private constant _BID_TYPEHASH = 0xaeabe233a7566271b1a36b65cdc361c721effabfa0c6058288df81ece139e32d;
+    /// Bid(address maker,uint256 price,uint256 keyId,uint256 amount,uint256 nonce,uint256 startTime,uint256 endTime,uint256 protocolFee,Royalties[] royalties)Royalties(address receiver,uint256 fee)
+    bytes32 private constant _BID_TYPEHASH = 0x06c6c02a5759b468552d3e3ce52a9399e1ab78fb9194e4e0fed6b8f57aa1afee;
 
     /**
      * Function used to return the EIP712 hash of a order.
