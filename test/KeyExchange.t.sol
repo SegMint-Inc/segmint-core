169e7a49221c4ebf41e0d84d2e38d8da8416c340
ItsCuzzo <alan@cuzzolabs.io>
2023-12-21 13:57:30 +1100

    test: added to test for h02 resolution

--- test/KeyExchange.t.sol
+++ test/KeyExchange.t.sol
@@ -253,6 +253,48 @@ contract KeyExchangeTest is BaseTest {
         assertEq(users.eve.account.balance, expectedBalanceWithRoyalty);
     }
 
+    function test_ExecuteOrders_WithRoyalty_WrapsToWETHOnCallFail() public setKeyTerms(IKeyExchange.MarketType.FREE) {
+        IKeyExchange.Order memory order = getGenericOrder(users.alice.account);
+
+        /// Add a royalty payment of 2.5% to the order.
+        uint256 royaltyFee = order.price * 250 / 10_000;
+        order.royalties = new IKeyExchange.Royalties[](1);
+        order.royalties[0].receiver = users.eve.account;
+        order.royalties[0].fee = royaltyFee;
+
+        bytes32 orderHash = keyExchange.hashOrder(order);
+
+        IKeyExchange.OrderParams[] memory orders = new IKeyExchange.OrderParams[](1);
+        orders[0] = signOrder(order, users.alice.privateKey);
+
+        assertEq(keyExchange.orderStatus(orderHash), IKeyExchange.Status.OPEN);
+        assertEq(keys.balanceOf(users.alice.account, order.keyId), keySupply);
+        assertEq(keys.balanceOf(users.bob.account, order.keyId), 0);
+
+        uint256 initialMakerBalance = users.alice.account.balance;
+        uint256 initialFeeBalance = keyExchange.feeReceiver().balance;
+
+        // Mock a call so that the royalty transfer of native token fails.
+        vm.mockCallRevert({ callee: users.eve.account, msgValue: royaltyFee, data: "", revertData: "" });
+
+        hoax(users.bob.account, order.price);
+        vm.expectEmit({ checkTopic1: true, checkTopic2: false, checkTopic3: false, checkData: true });
+        emit OrderFilled({ orderHash: orderHash });
+        keyExchange.executeOrders{ value: order.price }(orders);
+
+        assertEq(keyExchange.orderStatus(orderHash), IKeyExchange.Status.FILLED);
+        assertEq(keys.balanceOf(users.alice.account, order.keyId), keySupply - order.amount);
+        assertEq(keys.balanceOf(users.bob.account, order.keyId), order.amount);
+
+        uint256 expectedFee = order.price * order.protocolFee / 10_000;
+        uint256 expectedEarnings = order.price - expectedFee - royaltyFee;
+
+        assertEq(users.alice.account.balance, initialMakerBalance + expectedEarnings);
+        assertEq(users.bob.account.balance, 0);
+        assertEq(keyExchange.feeReceiver().balance, initialFeeBalance + expectedFee);
+        assertEq(mockWETH.balanceOf(users.eve.account), royaltyFee);
+    }
+
     function testCannot_ExecuteOrders_ZeroAddressInvalid_RoyaltyReceiver() public setKeyTerms(IKeyExchange.MarketType.FREE) {
         IKeyExchange.Order memory order = getGenericOrder(users.alice.account);
 
